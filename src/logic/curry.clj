8155d7bd94b924a43083d0848e0581ba878312d0
Thiago <thiago.carvp@gmail.com>
2021-02-06 21:01:34 -0300

    Keep it clean

--- src/logic/curry.clj
+++ src/logic/curry.clj
@@ -1,25 +1,6 @@
 (ns logic.curry
   (:require [clojure.core :as core]))
 
-(comment
-  (c/defn my-fn
-          [arg1 arg2]
-          (str arg1 arg2))
-
-  (defn simple-map
-    [c-fn #_ "recebe 3 args"
-     list]
-    (map (c-fn arg1 arg2) list))
-
-  (my-sum
-    ([x] (partial my-sum x))
-    ([x y] (partial my-sum x y))
-    ([x y z] (+ x y z))))
-
-(defn my-sum
-  [x y z]
-  (+ x y z))
-
 (defmacro defn
   [fn-name args body]
   `(core/defn ~fn-name
@@ -27,18 +8,3 @@
             (map (fn [arg-count]
                    `(~(vec (take arg-count args)) (partial ~fn-name ~@(take arg-count args))))))
      (~args ~body)))
-
-(macroexpand (defn my-sum
-               [x y]
-               (+ x y)))
-
-(defn my-sum
-  [x y z a]
-  (+ x y z a))
-
-(as-> (my-sum 1) %
-      (map % [2 3 4])
-      (apply juxt %)
-      (% 10)
-      (apply juxt %)
-      (% 100))
